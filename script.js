92821862682bb0b7b0c8ece1059384f609e3e4fb
jefking <jefking@outlook.in>
2024-03-23 15:19:31 +0530

    all done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let count = document.querySelector("#count");
+let error = document.querySelector(".error");
 let decrement=document.querySelector("#decrement");
 let increment=document.querySelector("#increment");
 let clear=document.querySelector("#clear");
@@ -6,9 +7,11 @@ let x=0;
 decrement.addEventListener("click",()=>{
     x--;
     
-    if(x<=0){
+    if(x<0){
         decrement.disabled=true;
-        
+        error.innerHTML="Error : Cannot go below 0";
+        x=0;
+
     }else{
         count.innerText=x;
     }
@@ -16,6 +19,10 @@ decrement.addEventListener("click",()=>{
 increment.addEventListener("click",()=>{
     x++;
     count.innerText=x;
+    if(x==1){
+        decrement.disabled=false;
+        error.innerHTML="";
+    }
 });
 clear.addEventListener("click",()=>{
     x=0;
